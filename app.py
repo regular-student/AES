eb4248f5f52cdc3e48c35b4b559123bb9c7e7d71
regular-student <alanhacase@gmail.com>
2026-04-09 15:16:08 -0300

    added:pbkdf2

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
 import os
 from aes import encrypt_cbc
+from pbkdf2 import gerar_chave_pbkdf2
 
-def testar_cbc():
+def main():
     print("teste")
     
     #mensagem qualquer, com tamanho "quebrado" (não é múltiplo de 16)
-    mensagem_texto = "Mine has been a life of much shame - Osamu Dazai!"
-    mensagem_bytes = mensagem_texto.encode('utf-8')
+    mensagem = "Mine has been a life of much shame - Osamu Dazai!"
+    senha_usuario = "12344"
     
-    print(f"Tamanho original da mensagem: {len(mensagem_bytes)} bytes")
+    print(f"1. Mensagem: '{mensagem}'")
+    print(f"2. Senha do usuário: '{senha_usuario}'\n")
     
-    #chave fixa de 32 bytes para o teste
-    chave = bytes([0x42] * 32) 
-    
+    chave_mestra, salt_gerado = gerar_chave_pbkdf2(senha_usuario)
     #vetor inicial aleatorio precisa ter exatos 16 bytes. 
     #os.urandom(16) gera bytes aleatórios de forma segura :)
     iv = os.urandom(16)
     
     #passa tudo pelo CBC
-    texto_cifrado = encrypt_cbc(mensagem_bytes, chave, iv)
+    texto_cifrado = encrypt_cbc(mensagem.encode('utf-8'), chave_mestra, iv)
     
-    print(f"Tamanho do texto cifrado: {len(texto_cifrado)} bytes")
-    print("\nTexto Cifrado (Hexadecimal):")
-    print(texto_cifrado.hex())
+    print(f"SALT (Guardar com o arquivo): {salt_gerado.hex()}")
+    print(f"IV   (Guardar com o arquivo): {iv.hex()}")
+    print(f"Cifra final ({len(texto_cifrado)} bytes): {texto_cifrado.hex()}")
 
 if __name__ == "__main__":
-    testar_cbc()
+    main()
